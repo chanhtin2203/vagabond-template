a450b1a3d6cee36f49251ebc3284021875cd5e75
chanhtin2203 <56529818+chanhtin2203@users.noreply.github.com>
2022-12-05 10:09:02 +0700

    .

--- src/Hooks/useConnectSocket.js
+++ src/Hooks/useConnectSocket.js
@@ -4,7 +4,7 @@ function useConnectSocket(io) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socketIO = io("https://vagabond-api.onrender.com", {
+    const socketIO = io("https://vagabond-api-production.up.railway.app", {
       transports: ["websocket"],
       secure: true,
     });
